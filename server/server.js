45773ce1ff22f738094a1584dfbf98fead09eefd
FedorowPavel <pavel.fedorov@exposit.com>
2022-08-02 14:29:57 +0300

    added room exist and full logic

--- server/server.js
+++ server/server.js
@@ -19,9 +19,13 @@ app.get('/api/room-exists/:roomId', (req,res) => {
   const {roomId} = req.params
   const room = rooms.find(room => room.id === roomId)
   if(room) {
-
+    if(room.connectedUsers.length > 3) {
+      return res.send({roomExists: true, full: true})
+    } else {
+      return res.send({roomExists: true, full: false})
+    }
   } else {
-
+    return res.send({roomExists: false})
   }
 })
 
